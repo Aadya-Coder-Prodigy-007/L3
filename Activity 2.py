1f63eb128713c0a9d49fa488329931308e3da4f7
Aadya-Coder-Prodigy-007 <aadyalingwal@gmail.com>
2025-12-11 19:35:17 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,17 @@
+ninteen=19
+number=float(ninteen)
+print("number is", type(number))
+decimal=12.34
+decimal=int(decimal)
+print("decimal is", type(decimal))
+word="23457678"
+word=int(word)
+print("word is", type(word))
+boolean=True
+boolean=str(boolean)
+print("boolean is",type (boolean))
+number=str(ninteen)
+print("number is", type(number))
+decimal=str(decimal)
+print("decimal is", type(decimal))
+
